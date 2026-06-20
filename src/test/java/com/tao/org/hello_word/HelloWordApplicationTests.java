2cff9a9acf4d5abf7b29238bf1065058ae2741a3
shiva3110 <shivaskd.v107@gmail.com>
2026-06-20 14:29:00 +0530

    initial code

---
+++ src/test/java/com/tao/org/hello_word/HelloWordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tao.org.hello_word;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
